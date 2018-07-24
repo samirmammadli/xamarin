3f1ef6193b90c70b7b96bd24fa6bb0639f6ce423
Samir Mammadli <mammadli.s86@gmail.com>
2018-07-24 20:16:36 +0400

    1

--- RemoteProcessManager/RemoteProcessManager/RemoteProcessManager.Android/MainActivity.cs
+++ RemoteProcessManager/RemoteProcessManager/RemoteProcessManager.Android/MainActivity.cs
@@ -1,27 +1,27 @@
-﻿using System;
+﻿//using System;
 
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
+//using Android.App;
+//using Android.Content.PM;
+//using Android.Runtime;
+//using Android.Views;
+//using Android.Widget;
+//using Android.OS;
 
-namespace RemoteProcessManager.Droid
-{
-    [Activity(Label = "RemoteProcessManager", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+//namespace RemoteProcessManager.Droid
+//{
+//    [Activity(Label = "RemoteProcessManager", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+//    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+//    {
+//        protected override void OnCreate(Bundle bundle)
+//        {
+//            TabLayoutResource = Resource.Layout.Tabbar;
+//            ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+//            base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
-    }
-}
+//            global::Xamarin.Forms.Forms.Init(this, bundle);
+//            LoadApplication(new App());
+//        }
+//    }
+//}
 
